6e11440826d2267c6446627db0a904b886e5c508
Iain Olliver <iain.olliver@gmail.com>
2020-12-19 14:55:43 +0000

    working version of events

---
+++ events.py
@@ -0,0 +1,252 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+import random
+
+NULL = "NULL"
+ATTACK_START = "ATTACK_START"
+THROUGH_BALL = "THROUGH_BALL"
+ONE_ON_ONE = "ONE_ON_ONE"
+LONG_SHOT = "LONG_SHOT"
+GOAL_SCORED = "GOAL_SCORED"
+NORMAL_SAVE = "NORMAL_SAVE"
+GREAT_SAVE = "GREAT_SAVE"
+POST_HIT = "POST_HIT"
+SHOT_OVER = "SHOT_OVER"
+KICK_OFF = "KICK_OFF"
+GOAL_KICK = "GOAL_KICK"
+
+EVENTS = {}
+
+def register(cls):
+	EVENTS[cls.KEY] = cls
+	return cls
+
+class Event:
+	KEY = "TBC" # identifier for this event
+	PARAMS = {} # required to describe this event
+	PRECEDES = [] # what events can follow this event
+	TEXTS = [] # commentary to display
+	UPDATES = {} # important changes to update on the Match object
+
+	def __init__(self, **kwargs):
+		for k in self.PARAMS:
+			assert k in kwargs, "{} was not given param {}".format(self.KEY, k)
+		self.params = dict(**kwargs)
+
+	def __str__(self):
+		# for example: "ATTACK_START:team=mancity"
+		return "{}:{}".format(
+			self.KEY,
+			",".join((
+				"{}={}".format(k, v)
+				for k, v in self.params.items()
+			)),
+		)
+
+	def generate_next(self, **kwargs):
+		"""Create the next Event based on self (previous event) + some match kwargs"""
+		logger.debug("Generating next event after: %s", self.KEY)
+		next_event_key = random.choice(self.PRECEDES)
+		logger.debug("Chosen next event KEY: %s", next_event_key)
+		match_kwargs = dict(**kwargs)
+		# based on this event's params, generate params for next event
+		if next_event_key == NULL:
+			# forget all previous parameters
+			next_event_params = {}
+		else: # pass on all params from previous event
+			next_event_params = match_kwargs
+			next_event_params.update(self.params)
+			logger.debug("Remembering previous params from %s for %s: %s", self.KEY, next_event_key, next_event_params)
+			next_event_params.update(self.get_next_params(next_event_key, **match_kwargs))
+		logger.debug("Got next params for %s: %s", next_event_key, next_event_params)
+		next_event_cls = EVENTS[next_event_key]
+		next_event_instance = next_event_cls(**next_event_params)
+		print(next_event_instance.get_text())
+		return next_event_instance
+
+	@classmethod
+	def get_params(cls, **kwargs):
+		"""Get required event params from match kwargs"""
+		return {}
+
+	def get_next_params(self, next_event_key, **kwargs):
+		"""Given the event that will follow this (+ match kwargs), get the params for that event"""
+		match_kwargs = dict(kwargs)
+		return match_kwargs
+
+	def get_text(self):
+		if not self.TEXTS:
+			return ""
+		try:
+			return random.choice(self.TEXTS).format(**self.params)
+		except KeyError as e:
+			logger.error("Event missing param %s: self.params=%s", e, self.params)
+			raise e
+
+@register
+class NullEvent(Event):
+	KEY = NULL
+	PRECEDES = [NULL, ATTACK_START]
+
+	def get_next_params(self, next_event_key, **kwargs):
+		if next_event_key == ATTACK_START:
+			return {
+				"team": random.choice([kwargs["home_team"], kwargs["away_team"]]),
+			}
+		return {}
+
+@register
+class AttackStartEvent(Event):
+	KEY = ATTACK_START
+	PARAMS = {'team'}
+	PRECEDES = [THROUGH_BALL, LONG_SHOT]
+	TEXTS = [
+		"{team} launches an attack...",
+		"{team} are on the attack...",
+	]
+
+	def get_next_params(self, next_event_key, **kwargs):
+		if next_event_key == LONG_SHOT:
+			return {
+				"player": self.params["team"].get_random_player(),
+			}
+		if next_event_key == THROUGH_BALL:
+			return {
+				"from_player": self.params["team"].get_random_player(),
+				"to_player": self.params["team"].get_random_player(),
+			}
+		return {}
+
+@register
+class ThroughBallEvent(Event):
+	KEY = THROUGH_BALL
+	PARAMS = {'from_player', 'to_player'}
+	PRECEDES = [ONE_ON_ONE]
+	TEXTS = [
+		"{from_player} plays a through ball to {to_player}...",
+	]
+
+	def get_next_params(self, next_event_key, **kwargs):
+		if next_event_key == ONE_ON_ONE:
+			return {
+				"player": self.params["team"].get_random_player(),
+			}
+		return {}
+
+@register
+class LongShotEvent(Event):
+	KEY = LONG_SHOT
+	PARAMS = {'player'}
+	PRECEDES = [GOAL_SCORED, GREAT_SAVE, POST_HIT, SHOT_OVER, NORMAL_SAVE]
+	TEXTS = [
+		"{player} shoots from distance...",
+	]
+
+	def get_next_params(self, next_event_key, **kwargs):
+		return {
+			"goalkeeper": "the goalkeeper",
+		}
+
+@register
+class OneOnOneEvent(Event):
+	KEY = ONE_ON_ONE
+	PARAMS = {'player'}
+	PRECEDES = [GOAL_SCORED, GREAT_SAVE, POST_HIT, SHOT_OVER]
+	TEXTS = [
+		"{player} is one-on-one with the keeper!",
+	]
+
+	def get_next_params(self, next_event_key, **kwargs):
+		return {
+			"goalkeeper": "the goalkeeper",
+		}
+
+@register
+class ShotOverEvent(Event):
+	KEY = SHOT_OVER
+	PARAMS = {'player', 'team'}
+	PRECEDES = [GOAL_KICK]
+	TEXTS = [
+		"Over the bar",
+	]
+
+	def get_next_params(self, next_event_key, **kwargs):
+		home_team = kwargs["home_team"]
+		return {
+			"team": kwargs["home_team"] if self.params["team"] == kwargs["away_team"] else kwargs["away_team"],
+		}
+
+@register
+class PostHitEvent(Event):
+	KEY = POST_HIT
+	PARAMS = {'player'}
+	PRECEDES = [GOAL_KICK]
+	TEXTS = [
+		"{player} hits the post!",
+		"Off the post!",
+	]
+
+	def get_next_params(self, next_event_key, **kwargs):
+		home_team = kwargs["home_team"]
+		return {
+			"team": kwargs["home_team"] if self.params["team"] == kwargs["away_team"] else kwargs["away_team"],
+		}
+
+@register
+class NormalSaveEvent(Event):
+	KEY = NORMAL_SAVE
+	PARAMS = {'player', 'goalkeeper'}
+	PRECEDES = [NULL]
+	TEXTS = [
+		"Easy save for {goalkeeper}",
+	]
+
+@register
+class GreatSaveEvent(Event):
+	KEY = GREAT_SAVE
+	PARAMS = {'player', 'goalkeeper'}
+	PRECEDES = [NULL]
+	TEXTS = [
+		"Great save by {goalkeeper}!",
+	]
+
+@register
+class GoalScoredEvent(Event):
+	KEY = GOAL_SCORED
+	PARAMS = {'player', 'team'}
+	PRECEDES = [KICK_OFF]
+	TEXTS = [
+		"Goal for {team}!",
+		"{player} scores for {team}!",
+		"{player} scores!",
+	]
+	UPDATES = {'goal_scored'}
+
+	def get_next_params(self, next_event_key, **kwargs):
+		home_team = kwargs["home_team"]
+		return {
+			"team": kwargs["home_team"] if self.params["team"] == kwargs["away_team"] else kwargs["away_team"],
+		}
+
+
+@register
+class KickOffEvent(Event):
+	KEY = KICK_OFF
+	PARAMS = {'team'}
+	PRECEDES = [NULL]
+	TEXTS = [
+		"{team} kick off",
+	]
+
+@register
+class GoalKickEvent(Event):
+	KEY = GOAL_KICK
+	PARAMS = {'team'}
+	PRECEDES = [NULL]
+	TEXTS = [
+		"Goal kick to {team}",
+	]
+
+for key, event in EVENTS.items():
+	assert event.PRECEDES, "Event {} does not precede any other event - please add at least one event that may follow {} (else we will get stuck!)".format(key, key)
---
+++ match.py
@@ -0,0 +1,77 @@
+import events
+import logging
+import time
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+from team import get_matchup
+
+class Match:
+	def __init__(self, home_team, away_team):
+		self.home_team = home_team
+		self.away_team = away_team
+		self.home_score = 0
+		self.away_score = 0
+		self.minute = 0
+		self.events = [] # (event, minute)
+
+	@property
+	def score(self):
+		return "{} {} - {} {}".format(self.home_team, self.home_score, self.away_score, self.away_team)
+		
+	def start(self, tick_period=0):
+		logger.info("Kick off! %s vs %s is underway", self.home_team, self.away_team)
+		self.events.append((events.KickOffEvent(team=self.home_team), 0))
+		while self.minute < 90:
+			self.tick()
+			time.sleep(tick_period)
+		
+		logger.info("The referee blows the final whistle!")
+		logger.info("Final score: %s", self.score)
+
+	def tick(self):
+		logger.debug("Tick: minute %s event %s", self.minute, self.events[-1])
+		self.minute += 1
+		print("Minute:", self.minute)
+		last_event, last_event_minute = self.events[-1]
+		new_event = last_event.generate_next(home_team=self.home_team, away_team=self.away_team)
+		if "goal_scored" in new_event.UPDATES:
+			logger.debug("goal scored, params=%s", new_event.params)
+			if new_event.params["team"] == self.home_team:
+				self.home_score += 1
+			if new_event.params["team"] == self.away_team:
+				self.away_score += 1
+			logger.info("%s", self.score)
+		self.events.append((new_event, self.minute))
+
+	def get_summary(self):
+		# We have stored all events so we can find goals, cards etc
+		scorers = []
+		for event, minute in self.events:
+			if "goal_scored" in event.UPDATES:
+				scorers.append("{} '{}".format(event.params["player"], minute))
+		print(self.score)
+		for scorer in scorers:
+			print(scorer)
+
+if __name__== '__main__':
+	number_of_games = 1
+	# game_speed: 0 is fastest, 0.5-1.0 is relatively normal, 5 is really slow
+	game_speed = 0.5
+	scores = []
+	total_home = 0
+	total_away = 0
+	for _ in range(number_of_games):
+		home_team, away_team = get_matchup()
+		home_team.load_players()
+		away_team.load_players()
+		match = Match(home_team, away_team)
+		match.start(game_speed)
+		scores.append(match.score)
+		total_home += match.home_score
+		total_away += match.away_score
+		match.get_summary()
+	if number_of_games > 1:
+		for score in scores:
+			print(score)
+		logger.info("Average home-away score over %s games: Home %.1f - %.1f Away ", number_of_games, total_home / number_of_games, total_away / number_of_games)
